24d6449423bb670405147d18c25ef4e9e0740375
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2026-02-10 13:51:20 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,6 +76,16 @@ with col1:
         if not nombre or not codigo:
             st.error("Nombre y código son obligatorios")
         else:
+            st.write({
+                "nombre": nombre,
+                "codigo": codigo,
+                "telefono": telefono,
+                "correo": correo,
+                "calle": calle,
+                "lote": lote,
+                "manzana": manzana,
+                "tipo_cobro": tipo_cobro
+            })
             supabase.table("cliente").insert({
                 "nombre": nombre,
                 "codigo": codigo,
@@ -86,6 +96,8 @@ with col1:
                 "manzana": manzana,
                 "tipo_cobro": tipo_cobro
             }).execute()
+
+            
 
             st.success("✅ Cliente registrado")
             st.rerun()
