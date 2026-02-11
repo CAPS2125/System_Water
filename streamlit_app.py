8a919fb39aea9e08210bc1ce001b75fd3b2ce626
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2026-02-10 16:09:43 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,15 +87,14 @@ with col2:
         )
         """).execute().data
     df = pd.DataFrame([
-    {
-        "Nombre": c["nombre"],
-        "Código": c["codigo"],
-        "Tipo": c["tipo_cobro"],
-        "Estatus": c["estado"]["estatus"] if c["estado"] else None,
-        "Saldo": c["estado"]["saldo"] if c["estado"] else 0,
-        "Adeudo": c["estado"]["adeudo"] if c["estado"] else 0
-    }
-    for c in clientes
-])
+        {
+            "Nombre": c["nombre"],
+            "Código": c["codigo"],
+            "Tipo": c["tipo_cobro"],
+            "Estatus": c["estado"]["estatus"] if c["estado"] else None,
+            "Saldo": c["estado"]["saldo"] if c["estado"] else 0,
+            "Adeudo": c["estado"]["adeudo"] if c["estado"] else 0
+        }
+        for c in clientes
+    ])
     st.dataframe(df, use_container_width=True)
-
